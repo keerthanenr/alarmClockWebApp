b62921864781e19382f6a635308a71b875b56f22
Keerthanen <keerthanenr@live.co.uk>
2024-01-07 19:08:44 +0000

    ALARMCLOCK-3: Adding the frontend fix3

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports"; // Import the configuration
+
+Amplify.configure(awsconfig); // Configure Amplify with the imported settings
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
